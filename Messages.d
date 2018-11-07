582fd0b47d2c5488d5f82fd2616d8dd7c55350d3
Fernode <smirkogm@gmail.com>
2018-11-07 22:28:48 +0100

    Messages.d with their static class

---
+++ Messages.d
@@ -0,0 +1,11 @@
+module messages;
+
+class Messages
+{
+	public static string NoArgumentMessage = "You must use arguments!\n\n*.exe inputString outputFile mode\ninputString = Any String; OutputFile = Any String; Mode = 'E' = Encode or 'D' = Decode";
+	public static string SuccessfullyEncoded = "Successfully encoded!";
+	public static string SuccessfullyDecoded = "Successfully decoded!";
+	public static string FailedInEncodingString = "Failed in encoding string!";
+	public static string FailedInDecodingString = "Failed in decoding string!";
+	public static string UnknownOperator = "Unknown operator!";
+}
