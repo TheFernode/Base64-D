5bd09a06aa4e0f28fae7f5860c886ee29a097c0b
Fernode <smirkogm@gmail.com>
2018-11-07 22:32:51 +0100

    changed from class to string enumerator

--- Messages.d
+++ Messages.d
@@ -1,11 +1,11 @@
 module messages;
 
-class Messages
+enum Messages : string
 {
-	public static string NoArgumentMessage = "You must use arguments!\n\n*.exe inputString outputFile mode\ninputString = Any String; OutputFile = Any String; Mode = 'E' = Encode or 'D' = Decode";
-	public static string SuccessfullyEncoded = "Successfully encoded!";
-	public static string SuccessfullyDecoded = "Successfully decoded!";
-	public static string FailedInEncodingString = "Failed in encoding string!";
-	public static string FailedInDecodingString = "Failed in decoding string!";
-	public static string UnknownOperator = "Unknown operator!";
+	NoArgumentMessage = "You must use arguments!\n\n*.exe inputString outputFile mode\ninputString = Any String; OutputFile = Any String; Mode = 'E' = Encode or 'D' = Decode",
+	SuccessfullyEncoded = "Successfully encoded!",
+	SuccessfullyDecoded = "Successfully decoded!",
+	FailedInEncodingString = "Failed in encoding string!",
+	FailedInDecodingString = "Failed in decoding string!",
+	UnknownOperator = "Unknown operator!"
 }
