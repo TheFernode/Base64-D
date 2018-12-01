53e34e72a95a12af94fd6d6b1fa55529ee658707
Fernode <smirkogm@gmail.com>
2018-12-01 01:01:53 +0100

    Minor updates

--- Messages.d
+++ Messages.d
@@ -1,11 +1,11 @@
 module messages;
 
-enum Messages : string
+static class Messages
 {
-	NoArgumentMessage = "You must use arguments!\n\n*.exe inputString outputFile mode\ninputString = Any String; OutputFile = Any String; Mode = 'E' = Encode or 'D' = Decode",
-	SuccessfullyEncoded = "Successfully encoded!",
-	SuccessfullyDecoded = "Successfully decoded!",
-	FailedInEncodingString = "Failed in encoding string!",
-	FailedInDecodingString = "Failed in decoding string!",
-	UnknownOperator = "Unknown operator!"
+	static const NoArgumentMessage = "You must use arguments!\n\n*.exe inputString outputFile mode\ninputString = Any String; OutputFile = Any String; Mode = 'E' = Encode or 'D' = Decode";
+	static const SuccessfullyEncoded = "Successfully encoded!";
+	static const SuccessfullyDecoded = "Successfully decoded!";
+	static const FailedInEncodingString = "Failed in encoding string!";
+	static const FailedInDecodingString = "Failed in decoding string!";
+	static const UnknownOperator = "Unknown operator!";
 }
